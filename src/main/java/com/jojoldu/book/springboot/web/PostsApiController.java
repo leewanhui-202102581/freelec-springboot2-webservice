0ff49439728ad9784424605f1d47b82abf10b736
leewanhui-202102581 <dhksgml0104@naver.com>
2024-01-16 19:47:41 +0900

    ~p.137 중반/

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -2,11 +2,11 @@ package com.jojoldu.book.springboot.web;
 
 
 import com.jojoldu.book.springboot.service.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RequiredArgsConstructor
@@ -18,5 +18,16 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
 
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id)
+    {
+        return postsService.findById(id);
     }
 }
