2ff88f9d52dac55031fdc63a48d1d129233a15fe
leewanhui-202102581 <dhksgml0104@naver.com>
2024-06-02 16:21:14 +0900

    github action으로 9과 CI 진행 중-3/ scripts/deploy.sh 커밋

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -3,12 +3,20 @@ package com.jojoldu.book.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.
         SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //삭제@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
+    //public static void main(String[] args) {
+    //    SpringApplication.run(Application.class, args);
+    //}
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
+
     }
 }
